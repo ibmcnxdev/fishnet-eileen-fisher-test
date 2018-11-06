f0d5ceabce183ceb2384c4cc9342ad5ea8d5dd13
fishnet <klaporta@users.noreply.github.com>
2018-11-06 15:07:55 -0500

    Update newCommunity.js

--- newCommunity.js
+++ newCommunity.js
@@ -1,4 +1,4 @@
+document.getElementById("placeButton").value="Request Community";
 document.getElementById("placeButton").onclick = function(){
   location.href="https://feb-0uxem8.mybluemix.net/forms/landing/org/app/cee00bdb-a3e2-4289-823b-8da02ee41469/launch/index.html?form=F_CommunityRequest1";
   };
-  
