37fe285298cd32fe4c525b9520807196f759ae79
fishnet <klaporta@users.noreply.github.com>
2018-11-06 15:22:13 -0500

    Update newCommunity.js

--- newCommunity.js
+++ newCommunity.js
@@ -1,5 +1,6 @@
 document.getElementById("placeButton").onclick = function(){
-  location.href="https://feb-0uxem8.mybluemix.net/forms/landing/org/app/cee00bdb-a3e2-4289-823b-8da02ee41469/launch/index.html?form=F_CommunityRequest1";
-  };
+/*  location.href="https://feb-0uxem8.mybluemix.net/forms/landing/org/app/cee00bdb-a3e2-4289-823b-8da02ee41469/launch/index.html?form=F_CommunityRequest1";*/
+  window.open("https://feb-0uxem8.mybluemix.net/forms/landing/org/app/cee00bdb-a3e2-4289-823b-8da02ee41469/launch/index.html?form=F_CommunityRequest1", "_blank");
+};
 
 document.getElementById("placeButton").textContent = "Request a Community";
