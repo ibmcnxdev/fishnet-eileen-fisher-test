92da25701330944ce23e9e91baa1f05b27b0b0e0
fishnet <klaporta@users.noreply.github.com>
2019-05-01 09:48:23 -0400

    Update newCommunity.js

--- newCommunity.js
+++ newCommunity.js
@@ -1,7 +1,7 @@
 /*document.getElementById("lconn-create-community-container").onclick = 
   window.open("https://feb-0uxem8.mybluemix.net/forms/landing/org/app/cee00bdb-a3e2-4289-823b-8da02ee41469/launch/index.html?form=F_CommunityRequest1", "_blank");
 ;*/
-document.getElementById("bx--dropdown-text").textContent = "Request a Community";
+document.getElementById("bx--dropdown-text").createTextNode("Request a Community");
 
 /*div = document.getElementById("lconn-create-community-container");
 txt = document.createTextNode("Request a Community");
